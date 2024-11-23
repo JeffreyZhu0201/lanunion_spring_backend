cb8494bfdfd930153053fa18c6425af0498ea321
JeffreyZhu <1624410543@qq.com>
2024-11-23 16:47:02 +0800

    init the project and add database info

---
+++ src/main/java/com/jeffrey/lanunion_spring_backend/LanunionSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jeffrey.lanunion_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanunionSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanunionSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeffrey/lanunion_spring_backend/LanunionSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffrey.lanunion_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanunionSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
